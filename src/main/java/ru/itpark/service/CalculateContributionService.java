67b34f1367a1098d49eede291c49e5dee9b96fb7
student <student@localhost>
2018-11-23 17:08:41 +0300

    renamed name

--- src/main/java/ru/itpark/service/CalculateContributionService.java
+++ src/main/java/ru/itpark/service/CalculateContributionService.java
@@ -3,7 +3,7 @@ package ru.itpark.service;
 public class CalculateContributionService {
     public int calculateContribution(int sum, int years, int percent) {
         int contribution = sum;
-        for (int b = 0; b < years; b++) {
+        for (int i = 0; i < years; i++) {
             contribution += ((contribution * percent) / 100) ;
         }
         return contribution;
