f963a3ef8c66e10eba987d2678653e57ccf0115f
student <student@localhost>
2018-11-23 16:56:02 +0300

    fixed typos in variable names

--- src/main/java/ru/itpark/service/CalculateContributionService.java
+++ src/main/java/ru/itpark/service/CalculateContributionService.java
@@ -1,12 +1,13 @@
 package ru.itpark.service;
 
 public class CalculateContributionService {
-    public int calculateContribution(int sum, int years, int precent) {
+    public int calculateContribution(int sum, int years, int percent) {
         int contribution = sum;
         for (int b = 0; b < years; b++) {
-            contribution = ((contribution * precent) / 100) + contribution;
+            contribution = ((contribution * percent) / 100) + contribution;
         }
         return contribution;
+
     }
 
 }
